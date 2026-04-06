a3762546d05d0419b08a8c37f4918fdfebe6cf34
Isaac Ferreira <zaclummys@gmail.com>
2026-04-06 16:57:40 -0300

    Implement sign up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,13 +1,43 @@
-import { Card } from '@/components/ui/card';
+import Link from 'next/link';
+
+import { Card, CardAction, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Field, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 export default function SignUp() {
     return (
-        <>
-            <Card className="w-full max-w-md mx-auto mt-10">
-                <span className="text-2xl font-bold mb-6">Sign Up</span>
+        <div className="min-h-screen flex items-center justify-center">
+            <Card className="w-full max-w-md">
+                <CardHeader>
+                    <CardTitle>Sign Up</CardTitle>
+                    <CardAction>
+                        <Link href="/sign-in">
+                            Already have an account?
+                        </Link>
+                    </CardAction>
+                </CardHeader>
 
-                M
+                <CardContent>
+                    <form className="flex flex-col gap-6">
+                        <Field>
+                            <FieldLabel htmlFor="email">Email</FieldLabel>
+                            <Input id="email" type="email" placeholder="Enter your email" />
+                        </Field>
+
+                        <Field>
+                            <FieldLabel htmlFor="password">Password</FieldLabel>
+                            <Input id="password" type="password" placeholder="Enter your password" />
+                        </Field>
+                    </form>
+                </CardContent>
+
+                <CardFooter>
+                    <Button type="submit" className="w-full">
+                        Sign Up
+                    </Button>
+                </CardFooter>
             </Card>
-        </>
+        </div>
     );
 }
