6c95d4d6c5afae09ad614df8c436d90e36d6f646
Isaac Ferreira <zaclummys@gmail.com>
2026-04-05 21:56:31 -0300

    Remove salesforce/cli

--- src/core/infra/prisma/deploy-result-repository.ts
+++ src/core/infra/prisma/deploy-result-repository.ts
@@ -1,11 +1,15 @@
-export default class PrismaDeployResultRepository implements DeployResultRepository {
-    private prisma: PrismaClient;
+import { DeployResult } from '@/core/domain/deploy-result';
+import DeployResultRepository from '@/core/application/interfaces/deploy-result-repository';
 
-    constructor (prisma: PrismaClient) {
+export default class PrismaDeployResultRepository implements DeployResultRepository {
+    private prisma: any;
+
+    constructor (prisma: any) {
         this.prisma = prisma;
     }
 
     public async saveDeployResult (deployResult: DeployResult): Promise<void> {
+        // TODO: Save component successes, component failures, test successes, and test failures
         await this.prisma.deployResult.create({
             data: {
                 id: deployResult.id,
