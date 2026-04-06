9dcf27d37ca3faad50a0dd19e1bcdeeb7221fa99
Isaac Ferreira <zaclummys@gmail.com>
2026-04-05 22:29:37 -0300

    Fix prisma

--- src/core/infra/prisma/client.ts
+++ src/core/infra/prisma/client.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const prisma = new PrismaClient();
 
-export { prisma };
+export default prisma;
