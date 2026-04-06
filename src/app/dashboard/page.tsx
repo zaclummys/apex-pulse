a370e9320dbc51c3c564924e03f5207386aa656c
Isaac Ferreira <zaclummys@gmail.com>
2026-04-06 17:41:51 -0300

    Create dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,94 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarHeader,
+    SidebarInset,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarProvider,
+    SidebarTrigger,
+} from '@/components/ui/sidebar';
+
+import {
+    Building,
+    LayoutDashboard,
+    Rocket,
+    Settings,
+    Zap,
+} from 'lucide-react';
+import { title } from 'process';
+
 export default function Dashboard() {
+    const items = [
+        {
+            title: 'Dashboard',
+            url: '/dashboard',
+            icon: <LayoutDashboard />,
+        },
+        {
+            title: 'Deployments',
+            url: '/deployments',
+            icon: <Rocket />,
+        },
+        {
+            title: 'Organizations',
+            url: '/organizations',
+            icon: <Building />,
+        },
+        {
+            title: 'Settings',
+            url: '/settings',
+            icon: <Settings />,
+        },
+    ];
+
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <h1 className="text-4xl font-bold">Welcome to the Dashboard!</h1>
-        </div>
+        <>
+            <SidebarProvider>
+                <Sidebar variant="inset">
+                    <SidebarHeader>
+                        <SidebarMenu>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton>
+                                    <Zap />
+
+                                    <span className="text-base font-semibold">
+                                        Apex Pulse
+                                    </span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarHeader>
+
+                    <SidebarContent>
+                        <SidebarGroup>
+                            <SidebarMenu>
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton>
+                                            {item.icon}
+
+                                            <span>
+                                                {item.title}
+                                            </span>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroup>
+                    </SidebarContent>
+                </Sidebar>
+
+                <SidebarInset>
+                    <div className="p-2">
+                        <SidebarTrigger />
+
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
+        </>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             lang="en"
             className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
         >
-            <body className="min-h-full flex flex-col dark bg-background text-foreground">
+            <body className="min-h-full flex flex-col bg-background text-foreground">
                 {children}
             </body>
         </html>
