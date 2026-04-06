ebd20eda50edb83c338049f19845704e11264068
Isaac Ferreira <zaclummys@gmail.com>
2026-04-06 16:44:51 -0300

    feat: initial commit

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from '@/components/ui/card';
+
+export default function SignUp() {
+    return (
+        <>
+            <Card className="w-full max-w-md mx-auto mt-10">
+                <span className="text-2xl font-bold mb-6">Sign Up</span>
+
+                M
+            </Card>
+        </>
+    );
+}
