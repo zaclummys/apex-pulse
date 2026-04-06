09365954d44580ed10b04f4ef6980a8bb6fb0529
Isaac Ferreira <zaclummys@gmail.com>
2026-04-05 22:28:01 -0300

    Update prisma client

--- src/core/infra/prisma/client.ts
+++ src/core/infra/prisma/client.ts
@@ -1,10 +1,5 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export { prisma };
