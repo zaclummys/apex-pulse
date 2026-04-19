0e5f9039163ecfde7dd29791cefa814d3d3f7d93
Isaac Ferreira <zaclummys@gmail.com>
2026-04-19 15:16:36 -0300

    Update sign in action

--- src/app/actions/commands/sign-in.ts
+++ src/app/actions/commands/sign-in.ts
@@ -1,16 +1,35 @@
 'use server';
 
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export default async function signInAction (state: any, formData: FormData) {
-    console.log('State:', state);
-    console.log('FormData:', formData);
+import { signIn } from '@/core';
 
+export default async function signInAction (state: any, formData: FormData) {
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
 
-    console.log('Email:', email);
-    console.log('Password:', password);
+    try {
+        const { token } = await signIn({ email, password });
+
+        const cookieStore = await cookies();
+
+        cookieStore.set('session_token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax',
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7,
+        });
+    } catch (error) {
+        console.error('An error occurred during sign-in:', error);
+
+        return {
+            errors: {
+                message: 'Invalid email or password',
+            },
+        };
+    }
 
     redirect('/dashboard');
 }
